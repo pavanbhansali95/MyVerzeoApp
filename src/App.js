fcbc72ddafd9c147c1756e2d098ccf464f7658af
pavan <pavanbhansali95@gmail.com>
2021-06-03 00:10:23 +0530

    modified changes

--- src/App.js
+++ src/App.js
@@ -13,45 +13,99 @@ import React from 'react';
 
 //state is coming which can be changed anywhere
 
+//conditinal stateents
+
 class App extends React.Component {
 
   constructor(props){
     
     super(props);
     this.state = {
-      'username': ''
+      'username': 'mark',
+      'userAge': 25,
+      'showMyContent': false,
+      'students': [
+        {
+          'id': 'abcde',
+          'marks': 40,
+          'name': 'shyam'
+        },
+        {
+          'id': 'abcdef',
+          'marks': 45,
+          'name': 'andrea'
+        },
+        {
+          'id': 'abcdeh',
+          'marks': 50,
+          'name': 'akshatha'
+        }
+      ]
     }
 
   }
 
-  onChangeHandler = (event) => {
+  onChangeHandler = name => {
+    alert(name);
+    //setstate will merge our state
     this.setState({
-      'username': event.target.value
-    })
+      'username': name
+    });
+    
 
   }
 
-  
+  toggleContentHandler = () => {
+    console.log('i am toggle content handler handler');
+    const isShow = this.state.showMyContent;
+    this.setState({'showMyContent':!isShow});
+    console.log('isShow--',this.state.showMyContent);
+  }
+
+//bind -> what is apply ,call,bind (mdn docs)
+//Two way binding
+//map opeartor in js??
+//filter opeartor -> task for the day
+
 
 
   render(){
+
     const {name,age,children} = this.props; //destructuring
+    // let myContent = null;
+
+    // if(this.state.showMyContent === true){
+    //   myContent = (<div>
+    //   <input type="text"  ></input>
+     
+    //  <button onClick = {() => {
+    //    this.onChangeHandler('alex')
+    //  }}>Click</button>
+    //  <h1>Hi i am {this.state.username} and my age is {this.state.userAge}</h1> 
+    //   </div>)
+    // } 
 
 
-    console.log('App props--',this.props);
-    console.log('App state---',this.state);
+ 
     return (
       <div>
-        <input type="text" onChange = {this.onChangeHandler} ></input>
-        <h1>Hi i am {this.state.username}</h1> 
-        {/* <h2> I am props children {children}</h2> */}
+       
+ { this.state.students.map(el=> {
+    return (
+      <div key={el.id} >
+        Hi my name is {el.name}
+        My Marks are {el.marks}
+      </div>
+    )
+  })
+}
 
       </div>
     )
   }
 
 
-}
+  }
 
 
 
